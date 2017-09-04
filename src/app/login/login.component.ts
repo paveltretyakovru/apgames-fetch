079b9f34327a0ca27cbd14bfeb65d5ac94ce2010
Pavel <tretyakovpavel.ru@gmail.com>
2017-09-04 03:15:16 +0300

    Created imitation of login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,9 +1,25 @@
 import { Store } from '@ngrx/store';
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+
+import { AuthData } from 'app/shared/models/auth-data.model';
+import { AuthService } from 'app/shared/auth.service';
 
 @Component({
   templateUrl: 'login.component.html',
 })
 export class LoginComponent {
-  authData: { login: string, password: string } = { login: '', password: '' };
+  authData: AuthData = { login: '', password: '' };
+
+  constructor(private authService: AuthService) {}
+
+  login(): any {
+    this.authService.login({
+      failed: (result: boolean) => { console.error('Failed', result) },
+      success: (result: boolean) => { console.info('Success', result) },
+      authData: this.authData,
+    }).subscribe((result: boolean) => {
+      console.info('Obser completed', result);
+    });
+  }
 }
--- src/app/shared/auth.service.ts
+++ src/app/shared/auth.service.ts
@@ -1,14 +1,35 @@
 import { Store } from '@ngrx/store';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
 
 import { AppState } from 'app/app-state.model';
+import { AuthData } from 'app/shared/models/auth-data.model';
 import { UserActions } from 'app/user/user.actions';
 
 @Injectable()
 export class AuthService {
   constructor(private store: Store<AppState>) {}
 
-  setUserAuth(value: boolean) {
+  setUserAuth(value: boolean): void {
     this.store.dispatch({ type: UserActions.SET_USER_AUTH, payload: value });
   }
+
+  login(options: {failed: any, success: any, authData: AuthData})
+    : Observable<boolean> {
+    
+      // Imitation of login
+      return Observable.of([true, false][Math.floor(Math.random()*2)])
+        .delay(1000)
+        .do((val: boolean) => {
+          
+          // Random currect login & password
+          if(val) {
+            options.success(val);
+            return true;
+          } else {
+            options.failed(val);
+            return false;
+          }
+        });
+    }
 }
