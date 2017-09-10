9bb2b8477b541cab2765c43e8e619bc505c1ee5c
Pavel <tretyakovpavel.ru@gmail.com>
2017-09-10 05:02:19 +0300

    Sources, currently fetching

--- src/backend/api/admin/admin.route.js
+++ src/backend/api/admin/admin.route.js
@@ -4,22 +4,27 @@ const router = express.Router();
 // Helpers
 const errorAction = require('../shared/errorAction');
 
+// Sources
+const SourcesService = require('../sources/sources.service');
+
 // Users
 const User = require('../user/user.model');
 const UserService = require('../user/user.service');
 
 // Initialize
 const userService = new UserService();
+const sourcesService = new SourcesService();
 
 // Get initial admin data
 router.get('/', (req, res) => {
   let usersP = userService.findAll;
+  let sourcesP = sourcesService.findAll;
 
-  Promise.all([usersP()])
+  Promise.all([usersP(), sourcesP()])
     .then(
       resolve => res.json({
         users: resolve[0],
-        sources: [],
+        sources: resolve[1],
       }),
       error => console.log('ERROR!', error)
     );
--- src/backend/api/sources/sources.service.js
+++ src/backend/api/sources/sources.service.js
@@ -1,11 +1,32 @@
+// const fetch = require('node-fetch');
+const request = require('request');
+
+const serverHost = 'http://apgames.ru/ru-ru'
+
 class SourcesService {
   consturctor() {
     console.log('Sources service');
   }
 
   findAll() {
-    return new Promise((req, res) => {
-      // fetching appgames api
+    var options = {
+      url: 'https://api.github.com/repos/request/request',
+      headers: {
+        'User-Agent': 'request'
+      }
+    };
+
+    return new Promise((resolve, reject) => {
+      request(SourcesService.apiRoutes.allSources, (err, response, body) => {
+        if(err) reject(err);
+        else resolve(JSON.parse(body).result.sources); 
+      });
     });
   }
 }
+
+SourcesService.apiRoutes = {
+  allSources: `${serverHost}/apiWmSources/?key=aSjIDQlqtjLDaTAB&format=json`
+};
+
+module.exports = SourcesService;
